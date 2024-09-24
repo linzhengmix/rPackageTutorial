c572648c1e47c5e91d92113be0e95adc3fd22512
RinLinux <mixfruitszu@gmail.com>
2024-09-24 22:36:10 +0800

    create man/*.rd

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,14 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+#' Title hello world
+#'
+#' @return Hello, world!
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' hello()
 hello <- function() {
   print("Hello, world!")
 }
