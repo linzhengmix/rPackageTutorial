536e48af1648d4a49a906ea8b6539d0377a4d37c
RinLinux <mixfruitszu@gmail.com>
2024-11-03 19:14:07 +0800

    add conteents.R

---
+++ R/contents.R
@@ -0,0 +1,45 @@
+
+
+#' @title Description of a data frame
+#' @description
+#' \code{contents} provides describes of the contents of a data frame.
+#'
+#'
+#' @param data a data frame.
+#'
+#' @importFrom utils object.size
+#'
+#' @return a list with 4 components:
+#'
+#' @export
+#'
+#' @examples
+#'
+contents <- function(data){
+  if(!is.data.frame(data)){
+    stop("You need to input a data frame")
+  }
+
+  dataname <- deparse(substitute(data))
+  size <- object.size(data)
+
+  varnames <- colnames(data)
+
+  colnames <- c("pos", "variable", "type", "n_unique", "n_miss", "pct_miss")
+  pos <- seq_along(data)
+  varname <- colnames(data)
+  type <- sapply(data, function(x) class(x)[1])
+  n_unique <- sapply(data, function(x)length(unique(x)))
+  n_miss <- sapply(data, function(x)sum(is.na(x)))
+  pct_miss <- n_miss / nrow(data)
+  varinfo <- data.frame(
+    pos, varname, type, n_unique, n_miss, pct_miss
+  )
+
+  results <- list(dfname=dataname, size=size,
+                  nrow=nrow(data), ncol=ncol(data),
+                  varinfo=varinfo)
+  class(results) <- c("contents","list")
+  return(results)
+
+}
---
+++ R/plot.R
@@ -0,0 +1,41 @@
+
+
+
+#' Title
+#'
+#' @param x an object of class \code{contents}.
+#' @param ... additional arguments
+#'
+#' @import ggplot2
+#'
+#' @importFrom stats reorder
+#'
+#' @method plot  contents
+#' @return a \code{ggplot2} graph
+#' @export
+#'
+#' @examples
+plot.contents <- function(x, ...){
+  if(!inherits(x, "contents")){
+    stop("Object must be of class 'contents'")
+  }
+
+  classes <- x$varinfo$type
+  variable = x$varinfo$varname
+  classes_n = as.numeric(as.factor(classes))
+
+  pct_n <- 100 * (1- x$varinfo$pct_miss)
+  df <- data.frame(variable = variable,
+                   classes = classes,
+                   pct_n = pct_n,
+                   classes_n = classes_n)
+
+  ggplot(df, aes(x=reorder(variable, classes_n), y=pct_n, fill=classes)) +
+    geom_bar(stat="identity") +
+    labs(x="",y="Percent Available",
+         caption=paste(x$nrow, "cases",x$ncol, "variables"), fill="Type") +
+    guides(fill=guide_legend(reverse=TRUE)) +
+    scale_y_continuous(breaks=seq(0, 100 ,20)) +
+    coord_flip() +
+    theme_minimal()
+}
